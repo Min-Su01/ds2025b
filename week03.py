39a428e084ea7671148e953db842deea00435fa7
Min-Su01 <tjalstn100@naver.com>
2025-03-24 12:13:43 +0900

    0.11 array

--- week03.py
+++ week03.py
@@ -1,9 +1,9 @@
-def intersection(l1, l2):
-    l3 = list()
-    for v in l1:
-        if v in l2:
-            l3.append(v)
-    return l3
+def interss(l1, l2):
+    s1 = set(l1)
+    s2 = set(l2)
+    #return list(s1.intersection(s2)) #set에서 지원해주는 intersection함수 사용
+    return list(s1 & s2)
+
 l1 = [45, 5, 22, 31, 7, 19]
 l2 = [2, 1, 5, 22, 7, 38, 19, 27, 13, 41]
-print(intersection(l1,l2))  #l1, l2의 중복값들 고르기
+print(interss(l1,l2))  #l1, l2의 중복값들 고르기
