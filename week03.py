aa06ce84bc3b74ca9d3480f37bbd633a44487f82
Min-Su01 <tjalstn100@naver.com>
2025-03-24 12:08:06 +0900

    0.10 array

--- week03.py
+++ week03.py
@@ -1,19 +1,9 @@
-def duplicate_city(cities):
-    result = list()
-    s = set()
-
-    for city in cities:
-        l1 = len(s)
-        s.add(city)
-        l2 = len(s)
-        if l1 == l2:
-            result.append(city)
-    return result
-
-
-#city = {'Incheon','Incheon' ,'Seoul', 'Gimpo'} #리스트나 튜플 등을 {} set에 주면 중복되는 것들 삭제
-cities = ['Incheon','Incheon','Seoul', 'Gimpo']  #set사용으로 중복 삭제
-cities.append('Anyang')
-cities.append('Seoul') #set사용으로 중복삭제!
-print(cities)
-print(set(duplicate_city(cities)))
+def intersection(l1, l2):
+    l3 = list()
+    for v in l1:
+        if v in l2:
+            l3.append(v)
+    return l3
+l1 = [45, 5, 22, 31, 7, 19]
+l2 = [2, 1, 5, 22, 7, 38, 19, 27, 13, 41]
+print(intersection(l1,l2))  #l1, l2의 중복값들 고르기
