711d46b1678a3d5670694f64850a233814d5c81c
Min-Su01 <tjalstn100@naver.com>
2025-03-24 11:56:29 +0900

    0.9 array

--- week03.py
+++ week03.py
@@ -1,7 +1,19 @@
+def duplicate_city(cities):
+    result = list()
+    s = set()
 
-#city = ['Incheon','Incheon' ,'Seoul', 'Gimpo'] #리스트나 튜플 등을 {} set에 주면 중복되는 것들 삭제
-city = {'Incheon','Incheon','Seoul', 'Gimpo'}  #set사용으로 중복 삭제
-city = set(city)
-city.add('Anyang')
-city.add('Seoul') #set사용으로 중복삭제!
-print(city)
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2:
+            result.append(city)
+    return result
+
+
+#city = {'Incheon','Incheon' ,'Seoul', 'Gimpo'} #리스트나 튜플 등을 {} set에 주면 중복되는 것들 삭제
+cities = ['Incheon','Incheon','Seoul', 'Gimpo']  #set사용으로 중복 삭제
+cities.append('Anyang')
+cities.append('Seoul') #set사용으로 중복삭제!
+print(cities)
+print(set(duplicate_city(cities)))
