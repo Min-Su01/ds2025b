1ee11b204137f307fb80f5e982dfc5e7a954c627
Min-Su01 <tjalstn100@naver.com>
2025-04-07 11:40:48 +0900

    v2.2 Stack

--- week05.py
+++ week05.py
@@ -25,8 +25,9 @@ class Stack:
 
 s1 = Stack()
 print(s1.pop())
-s1.push("자료구조")
-s1.push("데이터베이스")
-print(s1.pop())
-print(s1.pop())
-
+s1.push("Data structure")
+s1.push("Database")
+#print(s1.pop())
+#print(s1.pop())
+for i in range(3):
+    print(s1.pop())
