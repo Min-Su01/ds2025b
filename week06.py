ad7314187db35b92c79a78e2c532b82d8fe220a8
Min-Su01 <tjalstn100@naver.com>
2025-04-14 12:09:31 +0900

    v2.9 queue

--- week06.py
+++ week06.py
@@ -1,47 +1,8 @@
 from queue import Queue
-from typing import no_type_check_decorator
-
-
-class Node:
-    def __init__(self, data, link=None):
-        self.data = data
-        self.link = link
-
-
-class Queue:
-    def __init__(self):
-        self.front = None
-        self.rear = None
-        self.size = 0
-
-
-    def enqueue(self, data):
-        self.size = self.size + 1
-        node = Node(data)
-        if self.rear is None:
-            self.front = node
-            self.rear = node
-        else:
-            self.rear.link = node
-            self.rear = node
-
-
-    def dequeue(self):
-        if self.front is None:
-            raise IndexError("Queue is empty!")
-        self.size = self.size - 1
-        temp = self.front
-        self.front = self.front.link
-        if self.front is None:
-            self.rear = None
-        temp.link = None
-        return temp.data
 
 
 q = Queue()
-q.enqueue("Database")
-q.enqueue("Data Structure")
-print((q.size, q.front.data, q.rear.data))
-q.dequeue()
-q.dequeue()
-print((q.size, q.front, q.rear))
+q.put("Database")          #enqueue
+q.put("Data Structure")
+print(q.get())            #dequeue
+print(q.get())
