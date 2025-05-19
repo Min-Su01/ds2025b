552d00e501dbac30358f816058cba28ecb175a6c
Min-Su01 <tjalstn100@naver.com>
2025-05-19 22:43:19 +0900

    4.5 graph dfs

--- week11.py
+++ week11.py
@@ -8,7 +8,8 @@ graph = [
     [0, 0, 0, 1, 0, 0, 0, 1],
     [0, 0, 0, 0, 0, 0, 1, 0]
 ]
-
+#dfs(깊이 우선 탐색): 모든 노드를 방문하는 방법
+#깊이 우선 탐색이 너비 우선 탐색보다 좀 더 간단하다. (속도는 너비가 더 빠르다)
 def dfs(g, i, visited):
     visited[i] = 1
     print(chr(ord('A')+i), end=' ')
@@ -17,4 +18,4 @@ def dfs(g, i, visited):
             dfs(g, j, visited)
 
 visited_dfs = [0 for _ in range(len(graph))]
-dfs(graph, 7, visited_dfs)
+dfs(graph, 7, visited_dfs) #정점 7, 즉 'H'에서 탐색을 시작한다는 뜻.
