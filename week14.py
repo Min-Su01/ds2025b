3cad285803c07d971a18eacc63d5c7cd62d8b6c1
Min-Su01 <tjalstn100@naver.com>
2025-06-09 10:37:40 +0900

    v5.4 boj better

--- week14.py
+++ week14.py
@@ -1,15 +1,21 @@
-#백준 1929번
+import math
+import sys
+
 def is_prime(n) -> bool:
   if n <= 1:
       return False
+  elif n == 2:
+      return True
+  elif n % 2 == 0:
+      return False
   else:
-    for i in range(2, n):
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
         if n % i == 0:
           return False
   return True
 
-s, e = map(int, input().split())  #s, e의 입력값 넣기. 스플릿 사용하니 리스트로 반환됨
-                                 #만약 3. 16 넣으면 ['3', '16'] 으로 반환되는 것.
+#s, e = map(int, input().split())
+s, e = map(int, sys.stdin.readline().split())
 for i in range(s, e+1):
   if is_prime(i):
     print(i)
